f68101bb039564cb1907e9efffd209461ceb22eb
jnogt <117289604+jnogt@users.noreply.github.com>
2022-11-02 15:35:53 +0100

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Hello World!");
+		System.out.print("---***Hello World!***---\n");
 		int num=0;
 		num++;
 		System.out.printf("El valor es %d", num);
